4dc5f1184a9913035458f47291b2e5ef938df69a
Andrej Vano <avano@redhat.com>
2017-05-15 10:36:56 +0200

    [LPWrap] Save user,pw && generate

---
+++ keylogger.py
@@ -0,0 +1,36 @@
+"""
+Copyright (c) 2015, Aman Deep
+All rights reserved.
+
+
+A simple keylogger witten in python for linux platform
+All keystrokes are recorded in a log file.
+
+The program terminates when grave key(`) is pressed
+
+grave key is found below Esc key
+"""
+
+import pyxhook
+#change this to your log file's path
+log_file='/tmp/keys.log'
+
+#this function is called everytime a key is pressed.
+def OnKeyPress(event):
+  fob=open(log_file,'a')
+  if event.Key=='Return' or event.Key=='Control_L':
+    fob.close()
+    new_hook.cancel()
+    quit()
+  if event.Key=='Tab':
+    fob.write(event.Key)
+  else:
+    fob.write(chr(event.Ascii))
+  #instantiate HookManager class
+new_hook=pyxhook.HookManager()
+#listen to all keystrokes
+new_hook.KeyDown=OnKeyPress
+#hook the keyboard
+new_hook.HookKeyboard()
+#start the session
+new_hook.start()
--- lastpass-wrapper.sh
+++ lastpass-wrapper.sh
@@ -2,9 +2,14 @@
 ACCOUNT=$1
 ACTION=$2
 SCRIPT_HOME=$(dirname "$(readlink -f "$0")")
-source ${SCRIPT_HOME}/funcs.sh
-ID=$(get_window_id)
-URL=$(get_title_url ${ID})
+set -x
+get_window_id() {
+    echo "$(wmctrl -l | grep -oP "(0x\w+)(?=.* [C]hrome)")"
+}
+
+get_title_url() {
+    echo "$(xprop -id $1 | grep "_NET_WM_NAME" | perl -ne '/\".* - (www.*\.)?(.*\.[a-zA-Z]{2,3})/ && print "$2"')"
+}
 
 # Get username and id string for rofi
 get_username_id() {
@@ -25,8 +30,9 @@ input_username() {
 }
 
 # Print password and clear clipboard
-input_password() {
+paste_password() {
     lpass show -p $1 | xclip -selection clipboard && sleep .25; xdotool type "$(xclip -o -selection clipboard)" && xclip -selection clipboard -i /dev/null
+    exit 0
 }
 
 # Parse ID and username from the string (one line from rofi input will be selected and passed as output)
@@ -41,22 +47,38 @@ input_credentials() {
     if [ $# -lt 2 ]; then
         # Only input
         input_username $(lpass show --username ${URL})
-        input_password ${URL}
+        paste_password ${URL}
     else
         input_username $2
-        input_password $1
+        paste_password $1
     fi
 
 }
 
 get_user_for_url() {
-    USER=$(lpass show --username ${URL})
-    # If something went wrong, probably not logged in
-    if [ $? -ne 0 ]; then
-        lpass login ${ACCOUNT}
-        USER=$(lpass show --username ${URL})
-    fi
+    USER=$(lpass show --username ${URL} 2>&1)
     echo "${USER}"
+}
+
+get_id_for_url() {
+    sleep 1s
+    ID=$(lpass ls | grep ${URL})
+    if [[ $(echo ${ID} | wc -l) -ne 1 ]]; then
+        USER_ID=$(get_user_from_rofi)
+        echo ${USER_ID} | grep -oE "[0-9]{1,19}"
+    else
+        echo ${ID} | grep -oE "[0-9]{1,19}"
+    fi
+}
+
+get_user_from_rofi() {
+    USER_ID=$(get_username_id "$USER"| rofi -dmenu -p "Select account for $URL:")
+    if [[ ${USER_ID} == "Multiple matches found"* ]];
+        then
+            # Nothing was selected, exit
+            exit 0
+        fi
+    echo ${USER_ID}
 }
 
 get_user_string() {
@@ -64,19 +86,15 @@ get_user_string() {
 
     # Not found if empty, do nothing
     if [ -z "$USER" ]; then
-        exit 0
+        exit 1
     fi
 
     # If multiple matches found for the URL
     if [[ ${USER} == "Multiple matches found"* ]];
     then
-        USER_ID=$(get_username_id "$USER"| rofi -dmenu -p "Select account for $URL:")
-        if [[ ${USER_ID} == "Multiple matches found"* ]];
-        then
-            # Nothing was selected, exit
-            exit 0
-        fi
+        USER_ID=$(get_user_from_rofi)
     fi
+
     if [ -z "$USER_ID" ];
     then
         # Only one option
@@ -87,17 +105,19 @@ get_user_string() {
     fi
 }
 
-get_password() {
+input_password() {
     USER_STRING=$(get_user_string)
     if [ -z "${USER_STRING}" ];
     then
-        input_password ${URL}
+        paste_password ${URL}
+        exit 0
     else
-        input_password $(parse_id_username "${USER_STRING}")
+        paste_password $(parse_id_username "${USER_STRING}")
     fi
+    exit 0
 }
 
-get_user_password() {
+input_user_password() {
     USER_STRING=$(get_user_string)
     if [ -z "${USER_STRING}" ];
     then
@@ -107,19 +127,57 @@ get_user_password() {
     fi
 }
 
+record_credentials() {
+    rm -f /tmp/keys.log
+    python ${SCRIPT_HOME}/keylogger.py
+    CREDENTIALS=$(cat /tmp/keys.log)
+    USERNAME=$(echo ${CREDENTIALS%Tab*})
+    if [[ ! ${CREDENTIALS} == *"Tab"* ]]; then
+        # Only username supplied, pw will be generated
+        PASSWORD=$(lpass generate --sync=now ${URL} 15)
+        # The command above will add a new record, so delete it because we will re-add it later
+        lpass rm $(get_id_for_url ${URL})
+    else
+        PASSWORD=$(echo ${CREDENTIALS#*Tab})
+    fi
+
+    printf "Username: ${USERNAME}\nPassword: ${PASSWORD}\nURL: ${URL}" | lpass add --non-interactive ${URL}
+    unset PASSWORD
+}
+
+record() {
+    USER=$(lpass show --username ${URL})
+}
+
+test() {
+    echo "test"
+}
+check_lpass_session() {
+    if [ $? -ne 0 ]; then
+        if [[ ${USER} == *"Could not find specified account"* ]]; then
+            exit 1
+        fi
+        lpass login ${ACCOUNT}
+        USER=$(lpass show --username ${URL})
+        if [ $? -ne 0 ]; then
+            exit 1
+        fi
+    fi
+}
+
 main() {
+    check_lpass_session
+    ID=$(get_window_id)
+    URL=$(get_title_url ${ID})
     case "$2" in
-        "get-password")
-            get_password
+        "input-password")
+            input_password
         ;;
-        "get-user-password")
-            get_user_password
+        "input-user-password")
+            input_user_password
         ;;
-        "generate")
-            echo "create"
-        ;;
-        "record")
-            echo "record"
+        "record-credentials")
+            record_credentials
         ;;
         *)
             echo "You have failed to specify what to do correctly."
